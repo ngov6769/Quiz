00970d9fd136f159a763d0c988c8f29ca6227969
ngov6769 <ngov6769@gmail.com>
2022-09-09 00:41:48 +0700

    Commit 1

---
+++ app/src/main/java/com/example/appquiz/Ustil/Ustil.java
@@ -0,0 +1,13 @@
+package com.example.appquiz.Ustil;
+
+import com.example.appquiz.Model.DapAn;
+import com.example.appquiz.Model.TestQuiz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ustil {
+    public static List<DapAn> dapAn;
+    public static String name;
+    public static List<DapAn> dapAnChuan;
+}
